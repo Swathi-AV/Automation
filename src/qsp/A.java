bfb2ac56da3e03976effaa429a430907f54d0b76
Swathi-AV <43536491+Swathi-AV@users.noreply.github.com>
2018-09-24 15:40:40 +0530

    Initial commit

---
+++ src/qsp/A.java
@@ -0,0 +1,18 @@
+package qsp;
+
+import java.io.File;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class A {
+
+	public static void main(String[] args) throws Exception {
+		Workbook w = WorkbookFactory.create(new File("./data/Book.xlsx"));
+		String s=w.getSheet("sheet1").getRow(0).getCell(0).toString();
+		System.out.println(s);
+        
+	
+	}
+
+}
